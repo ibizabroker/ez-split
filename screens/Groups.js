20a4e781637de50d2f5c3361ed85e30775517fdc
tarun gowda <tarun.gowda2000@gmail.com>
2023-01-11 17:39:50 +0530

    list of groups

--- screens/Groups.js
+++ screens/Groups.js
@@ -1,15 +1,69 @@
-import { Text, View, StyleSheet, SafeAreaView } from 'react-native'
-import React, { useEffect } from 'react'
+import { Text, View, StyleSheet, SafeAreaView, ScrollView } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import AppBar from '../components/AppBar';
+import { useNavigation, useIsFocused } from '@react-navigation/native';
+import { ListItem } from '@rneui/themed';
 
 const Groups = () => {
+  const isFocused = useIsFocused();
+  const navigation = useNavigation();
+
+	const [groups, setGroups] = useState([]);
+
+  const fetchData = async () => {
+     await AsyncStorage.getItem('Groups')
+      .then(value => {
+        let groupsFetch = JSON.parse(value);
+        setGroups(groupsFetch);
+    })
+  }
+
+  useEffect(() => {
+    if(isFocused) {
+      fetchData();
+    }
+  }, [isFocused])
+
   return (
     <View style={styles.containerInitial}>
         <AppBar ez={'EZ '} split={'Split'} />
 
-        <SafeAreaView style={styles.container}>
-          <Text style={{fontFamily: 'Montserrat', color: '#D3D3D3'}}>Groups</Text>
-        </SafeAreaView>
+        {groups === null 
+          ? 
+            <View style={styles.container}>
+              <Text style={styles.noGroupsText}>You don't have any groups yet. Go to the Create Group tab to add a group.</Text>
+            </View>
+          :              
+            groups.map((item) => {
+              return (
+                <ScrollView
+                  key={item.title}
+                >
+                  <ListItem
+                    key={item.title}
+                    containerStyle={{
+                      backgroundColor: '#121212',
+                      borderBottomWidth: 2,
+                      borderBottomColor: '#332940',
+                    }}
+                  >
+                    <ListItem.Content>
+                      <ListItem.Title
+                        style={{
+                          fontFamily: 'Montserrat',
+                          fontSize: 16,
+                          color: '#D3D3D3',
+                          padding: 8,
+                          marginLeft: '5%'
+                        }}
+                      >{item.title}</ListItem.Title>
+                    </ListItem.Content>
+                  </ListItem>
+                </ScrollView>
+              )
+            })       
+        }   
 
       </View>
   )
@@ -24,6 +78,14 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    margin: '5%',
+    margin: '5%', 
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  noGroupsText: {
+    fontFamily: 'Montserrat',
+    fontSize: 16,
+    color: '#D3D3D3',
+    textAlign: 'center'
   }
 });
